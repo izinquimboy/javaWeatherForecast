8d8b717dca1c79c6b460ccae83c9b109fa38a1ab
Iziel <irinqum@ncsu.edu>
2024-06-20 11:05:04 -0400

    commit

--- src/weatherGUI.java
+++ src/weatherGUI.java
@@ -35,7 +35,9 @@ public class weatherGUI extends JFrame{
 
         setSize(800, 600);
 
-        setLocationRelativeTo(null);
+        //setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+        setLocation(1100, 100);
 
         setLayout(null);
 
@@ -45,36 +47,69 @@ public class weatherGUI extends JFrame{
     }
 
     private void addGuiComponents() {
+
+
         JLabel text = new JLabel();
         text.setBounds(10, 20, 651, 50);
-        text.setText("Enter\n" + "Location: ");
+        text.setText("Enter\n " + "Location: ");
         text.setFont(new Font("SansSerif", Font.BOLD, 24));
         add(text);
         
         JTextField searchText = new JTextField();
-        searchText.setBounds(201, 20, 540, 50);
+        searchText.setBounds(190, 20, 540, 50);
         searchText.setFont(new Font("Dialog", Font.PLAIN, 24));
         add(searchText);
 
         JButton search = new JButton(loadImage("javaWeatherForecast/src/assets/search.png"));
         search.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        search.setBounds(740, 20, 50, 50);
+        search.setBounds(732, 22, 50, 45);
         add(search);
 
         JLabel weatherConditionImg = new JLabel(loadImage("javaWeatherForecast/src/assets/cloudy.png"));
-        weatherConditionImg.setBounds(-50, 40, 450, 300);
+        weatherConditionImg.setBounds(-60, 20, 450, 300);
         add(weatherConditionImg);
 
-        JLabel weatherDesc = new JLabel();
-        weatherDesc.setText("Cloudy");
-        weatherDesc.setFont(new Font("SansSerif", Font.BOLD, 50));
-        weatherDesc.setBounds(350, -20, 600, 300);
+        JLabel tempText = new JLabel();
+        tempText.setText("100°F");
+        tempText.setFont(new Font("SansSerif", Font.BOLD, 50));
+        tempText.setBounds(310, -30, 600, 300);
+        add(tempText);
+
+        JLabel weatherDesc = new JLabel("Cloudy", SwingConstants.CENTER);
+        weatherDesc.setFont(new Font("SansSerif", Font.BOLD, 40));
+        weatherDesc.setBounds(10, 108, 300, 300);
+        weatherDesc.setHorizontalAlignment(JLabel.CENTER);
         add(weatherDesc);
 
-        JLabel tempText = new JLabel("100°F");
-        tempText.setFont(new Font("Monospaced", Font.PLAIN, 21));
-        tempText.setBounds(400, -20, 600, 300);
-        add(tempText);
+        JLabel feelsLikeText = new JLabel("<html><b>Feels like</b> 95°F</html>");
+        feelsLikeText.setFont(new Font("SansSerif", Font.PLAIN, 20));
+        feelsLikeText.setBounds(320, 15, 600, 300);
+        add(feelsLikeText);
+
+        JLabel precipText = new JLabel("<html><b>Preciptation</b> 2 in</html>");
+        precipText.setFont(new Font("SansSerif", Font.PLAIN, 20));
+        precipText.setBounds(320,45, 600, 300);
+        add(precipText);
+
+        JLabel windText = new JLabel("<html><b>Wind Speed</b> 3mph</html>");
+        windText.setFont(new Font("SansSerif", Font.PLAIN, 20));
+        windText.setBounds(320, 75, 600, 300);
+        add(windText);
+
+        JLabel humidText = new JLabel("<html><b>Humidity</b> 100%</html>");
+        humidText.setFont(new Font("SansSerif", Font.PLAIN, 20));
+        humidText.setBounds(320,105, 600, 300);
+        add(humidText);
+
+        JPanel daily = new JPanel();
+        daily.setBounds(550, 150, 225, 400);
+        daily.setBackground(new Color(198, 204, 207));
+        add(daily);
+
+        JPanel hourly = new JPanel();
+        hourly.setBounds(20, 320, 510, 230);
+        hourly.setBackground(new Color(198, 204, 207));
+        add(hourly);
 
         search.addActionListener(new ActionListener() {
 
